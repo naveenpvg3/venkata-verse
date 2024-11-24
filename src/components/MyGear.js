373a057b4cc30a04c8f707ff01de8a14ea1e8e7c
naveenpvg3 <naveenpvg3@gmail.com>
2024-11-23 19:15:10 -0500

    updated gear page

--- src/components/MyGear.js
+++ src/components/MyGear.js
@@ -29,32 +29,18 @@ const MyGear = () => {
     // Add more referral links here
   ];
 
-  const homeDecor = [
-    { name: "Home Decor Item 1", link: "https://example.com" },
-    { name: "Home Decor Item 2", link: "https://example.com" }
-    // Add more home decor products here
-  ];
-
-  const kitchen = [
-    { name: "Kitchen Product 1", link: "https://example.com" },
-    { name: "Kitchen Product 2", link: "https://example.com" }
+  const products = [
+    { name: "Vegetable Chopper", link: "https://www.amazon.com/Vegetable-Chopper-Multifunctional-Adjustable-Container/dp/B0D1BPJVQ7/ref=asc_df_B0D1BPJVQ7?mcid=e16300bab8193d3aa5676b8543f9796b&hvocijid=3269665196215396265-B0D1BPJVQ7-&hvexpln=73&tag=hyprod-20&linkCode=df0&hvadid=692875362841&hvpos=&hvnetw=g&hvrand=3269665196215396265&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9007584&hvtargid=pla-2281435180538&psc=1" },
+    { name: "Bug Zapper", link: "https://www.homedepot.com/p/Black-Flag-Portable-Handheld-Bug-Zapper-Insect-Killer-Racket-Black-ZR-7936-L/302273000?source=shoppingads&locale=en-US&pla&mtc=SHOPPING-BF-CDP-GGL-D28O-028_001_CHEMICALS-NA-NA-NA-PMAX-NA-NA-NA-NA-NBR-NA-NA-NA-PMAXONECLICK_JControl24&cm_mmc=SHOPPING-BF-CDP-GGL-D28O-028_001_CHEMICALS-NA-NA-NA-PMAX-NA-NA-NA-NA-NBR-NA-NA-NA-PMAXONECLICK_JControl24-20410431792--&gclsrc=aw.ds&gad_source=1&gbraid=0AAAAADq61Ufn-v82HK1YULre_dxp89HIs&gclid=CjwKCAiAl4a6BhBqEiwAqvrqutR90TDI_4WCGT_xry8ypUAcognJKzd0dEpzK6-O5NWhOsvBbRv2KxoCg20QAvD_BwE" }
     // Add more kitchen products here
-  ];
-
-  const gadgets = [
-    { name: "Gadget 1", link: "https://example.com" },
-    { name: "Gadget 2", link: "https://example.com" }
-    // Add more gadgets here
   ];
 
   return (
     <div className="my-gear">
-      <h2>My Gear/Recommended Products</h2>
-      <p>Here you can find my recommended products across various categories, complete with affiliate links.</p>
+      <h2>My Gear</h2>
+      <p>Here you can find best products which Venkata use personally.</p>
       <ProductSection title="Referrals" products={referrals} />
-      <ProductSection title="Home Decor" products={homeDecor} />
-      <ProductSection title="Kitchen" products={kitchen} />
-      <ProductSection title="Gadgets" products={gadgets} />
+      <ProductSection title="Products" products={kitchen} />
     </div>
   );
 };
